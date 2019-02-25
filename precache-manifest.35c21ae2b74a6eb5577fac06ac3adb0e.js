4b69d0942c95c7b89cb8e50be5412a5676f69f6a
AndriiDev96 <volyanskiy.2015@gmail.com>
2019-02-25 20:24:43 +0200

    Updates

---
+++ precache-manifest.35c21ae2b74a6eb5577fac06ac3adb0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "95f0732358e56d3ad078",
+    "url": "/to_do_app/static/js/runtime~main.95f07323.js"
+  },
+  {
+    "revision": "acaa0d50e28f9ec802a4",
+    "url": "/to_do_app/static/js/main.acaa0d50.chunk.js"
+  },
+  {
+    "revision": "10cc78529e37d235c77b",
+    "url": "/to_do_app/static/js/2.10cc7852.chunk.js"
+  },
+  {
+    "revision": "acaa0d50e28f9ec802a4",
+    "url": "/to_do_app/static/css/main.614caa67.chunk.css"
+  },
+  {
+    "revision": "10cc78529e37d235c77b",
+    "url": "/to_do_app/static/css/2.70a65a11.chunk.css"
+  },
+  {
+    "revision": "d99f26a898e0bfe95eddab391d06730c",
+    "url": "/to_do_app/index.html"
+  }
+];
