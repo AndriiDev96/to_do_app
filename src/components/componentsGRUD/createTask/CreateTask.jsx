47ae488c3c21ec24a80c59e4159cb29057bc069c
Andrii <avo@keepit.com>
2019-12-27 13:49:50 +0200

    Implemented some function for form which create new task

--- src/components/componentsGRUD/createTask/CreateTask.jsx
+++ src/components/componentsGRUD/createTask/CreateTask.jsx
@@ -4,60 +4,91 @@ import DatePicker from '../../datePicker/DataPicker'
 import './createTask.css';
 
 class CreateTask extends Component{
-
   state = {
-    modal: false
+    modal: true,
+    fieldName: '',
+    fieldType: '',
+    filedFinishDate: null
   }
 
-  toggle = () => {
+  stateModalWindow = () => {
     this.setState({
       modal: !this.state.modal
     });
   }
 
+  getFinishDate(date){
+    this.setState({
+      filedFinishDate: date
+    });
+  }
+
+  getNameTask = event => {
+    this.setState({
+      fieldName: event.target.value
+    });
+
+  }
+
+  getTypeTask = event => {
+    const value = event.target.value;
+
+    if (value.length <= 60) {
+      this.setState({
+        fieldType: event.target.value
+      });
+    }else{
+      console.log("value is > 60");
+    }
+    
+  }
+  
+
+  createTaskAsync = async (event) => {
+
+  }
+
   render() {
+    const { modal, fieldName, filedFinishDate} = this.state;
+    
     return (
       <MDBContainer>
         <MDBBtn
           color 
-          onClick={this.toggle} 
+          onClick={this.stateModalWindow} 
           className="btn-create-task mx-auto"
         >
           Create task
         </MDBBtn>
 
         <MDBModal
-            isOpen={this.state.modal}
-            toggle={this.toggle}
+            isOpen={modal}
+            toggle={this.stateModalWindow}
             size="md"
             cascading
         >
           <MDBModalHeader
-            toggle={this.toggle}
+            toggle={this.stateModalWindow}
             titleClass="d-inline title"
             className="text-center blue-gradient darken-3 white-text"
           >
             <MDBIcon className="mr-2" icon="pencil-alt" />
             Create task
           </MDBModalHeader>
-
           <MDBModalBody>
-            <MDBInput label="Name" icon="signature" />
-            <MDBInput label="Type" icon="tag" />
+            <MDBInput label="Name" icon="signature" type="text" maxLength="100" onChange={this.getNameTask} />
+            <MDBInput label="Type" icon="tag"type="text" maxLength="60" onChange={this.getTypeTask} />
             <div className="md-form">
               <i data-test="input" className="far fa-calendar-alt prefix"></i>
-              <DatePicker />
-              <label class="active">Finish date</label>
+              <DatePicker 
+                getDate={ this.getFinishDate.bind(this) }
+                finishDate={ filedFinishDate }
+              />
+              <label className="active">Finish date</label>
             </div>
 
             <div className="text-center mt-1-half">
-              <MDBBtn
-                color
-                className="mb-2 px-4"
-                onClick={this.toggle}
-              >
-                save
-              </MDBBtn>
+              <MDBBtn color className="mb-2 px-4" onClick={this.stateModalWindow}>save</MDBBtn>
             </div>
           </MDBModalBody>
 
--- src/components/datePicker/DataPicker.jsx
+++ src/components/datePicker/DataPicker.jsx
@@ -1,29 +1,19 @@
 import React, { Component } from "react";
 import BDatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import moment from "moment"
 import './datePicker.css';
+import moment from "moment"
 
 const  CURRENT_DAY = moment().toDate();
 
 class DatePicker extends Component {
-  state = {
-    startDate: CURRENT_DAY
-  };
-
-  handleChange = (date) => {
-    this.setState({
-      startDate: date
-    })
-  }
-
   render() {
-    
     return (
       <BDatePicker
         showPopperArrow={false}
-        selected={this.state.startDate}
-        onChange={this.handleChange}
+        selected={this.props.finishDate || CURRENT_DAY}
+        dateFormat = "dd/MM/yyyy"
+        onChange={this.props.getDate}
       />
     );
   }
